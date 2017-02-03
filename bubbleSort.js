4e8a7467ca7930f42c854adf8ad95526d909a4df
Jayan Zaman <jayanzaman@gmail.com>
2017-02-02 19:06:06 -0500

    counting outer and inner loops

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,13 +1,13 @@
 //bubble sort
 // array to sort
-var array = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8];
+// var array = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8];
 
-// swap function helper
-function swap(array, i, j) {
-  var temp = array[i];
-  array[i] = array[j];
-  array[j] = temp;
-}
+// // swap function helper
+// function swap(array, i, j) {
+//   var temp = array[i];
+//   array[i] = array[j];
+//   array[j] = temp;
+// }
 
 // // be careful: this is a very basic implementation which is nice to understand the deep principle of bubble sort (going through all comparisons) but it can be greatly improved for performances
 // function bubbleSortBasic(array) {
@@ -24,56 +24,56 @@ function swap(array, i, j) {
 // console.log(bubbleSortBasic(array.slice())); // => [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
 
 // correct implementation: this is the usual implementation of the bubble sort algorithm. Some loops execution are avoided if not they are not needed
-function bubbleSort(array) {
-  var swapped;
-  do {
-    swapped = false;
-    for(var i = 0; i < array.length; i++) {
-      if(array[i] && array[i + 1] && array[i] > array[i + 1]) {
-        swap(array, i, i + 1);
-        swapped = true;
-      }
-    }
-  } while(swapped);
-  return array;
-}
-
-console.log(bubbleSort(array.slice())); // => [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
-
-// // sample of arrays to sort
-// var arrayRandom = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8];
-// var arrayOrdered = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-// var arrayReversed = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
-
-// // swap function helper
-// function swap(array, i, j) {
-//   var temp = array[i];
-//   array[i] = array[j];
-//   array[j] = temp;
-// }
-
-// // be careful: this is a very basic implementation which is nice to understand the deep principle of bubble sort (going through all comparisons) but it can be greatly improved for performances
-// function bubbleSortBasic(array) {
-//   var countOuter = 0;
-//   var countInner = 0;
-//   var countSwap = 0;
-
-//   for(var i = 0; i < array.length; i++) {
-//     countOuter++;
-//     for(var j = 1; j < array.length; j++) {
-//       countInner++;
-//       if(array[j - 1] > array[j]) {
-//         countSwap++;
-//         swap(array, j - 1, j);
+// function bubbleSort(array) {
+//   var swapped;
+//   do {
+//     swapped = false;
+//     for(var i = 0; i < array.length; i++) {
+//       if(array[i] && array[i + 1] && array[i] > array[i + 1]) {
+//         swap(array, i, i + 1);
+//         swapped = true;
 //       }
 //     }
-//   }
-
-//   console.log('outer:', countOuter, 'inner:', countInner, 'swap:', countSwap);
+//   } while(swapped);
 //   return array;
 // }
 
-// bubbleSortBasic(arrayRandom.slice()); // => outer: 10 inner: 90 swap: 21
+// console.log(bubbleSort(array.slice())); // => [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
+
+// sample of arrays to sort
+var arrayRandom = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8];
+var arrayOrdered = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var arrayReversed = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+
+// swap function helper
+function swap(array, i, j) {
+  var temp = array[i];
+  array[i] = array[j];
+  array[j] = temp;
+}
+
+// be careful: this is a very basic implementation which is nice to understand the deep principle of bubble sort (going through all comparisons) but it can be greatly improved for performances
+function bubbleSortBasic(array) {
+  var countOuter = 0;
+  var countInner = 0;
+  var countSwap = 0;
+
+  for(var i = 0; i < array.length; i++) {
+    countOuter++;
+    for(var j = 1; j < array.length; j++) {
+      countInner++;
+      if(array[j - 1] > array[j]) {
+        countSwap++;
+        swap(array, j - 1, j);
+      }
+    }
+  }
+
+  console.log('outer:', countOuter, 'inner:', countInner, 'swap:', countSwap);
+  return array;
+}
+
+bubbleSortBasic(arrayRandom.slice()); // => outer: 10 inner: 90 swap: 21
 // bubbleSortBasic(arrayOrdered.slice()); // => outer: 10 inner: 90 swap: 0
 // bubbleSortBasic(arrayReversed.slice()); // => outer: 10 inner: 90 swap: 45
 
