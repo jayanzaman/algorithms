7bc4b4274b815e9247131b6db8a9942c79eec618
Jayan Zaman <jayanzaman@gmail.com>
2017-02-09 12:17:11 -0500

    how to set up postgres in windows

---
+++ postgresql/postgresql_setup.js
@@ -0,0 +1,10 @@
+// Open the Command Prompt as an Administrator: 
+// - Click Start -> search command prompt -> right click and choose "Run as Administrator"
+
+// @powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('http://s3.amazonaws.com/pgcentral/install.ps1'))"
+
+//   cd bigsql
+//   pgc install pg96
+//   pgc start pg96
+
+
