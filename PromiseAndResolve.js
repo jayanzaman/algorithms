b703a7615a9c70b5bf64d6b987f369ce86be501c
Jayan Zaman <jayanzaman@gmail.com>
2017-02-28 18:35:39 -0500

    promise and resolve

---
+++ PromiseAndResolve.js
@@ -0,0 +1,27 @@
+function resolveAfter2Seconds(x) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(x);
+    }, 2000);
+  });
+}
+
+async function add1(x) {
+  var a = resolveAfter2Seconds(20);
+  var b = resolveAfter2Seconds(30);
+  return x + await a + await b;
+}
+
+add1(10).then(v => {
+  console.log(v);
+});
+
+async function add2(x) {
+  var a = await resolveAfter2Seconds(20);
+  var b = await resolveAfter2Seconds(30);
+  return x + a + b;
+}
+
+add2(15).then(v => {
+  console.log(v);
+});
