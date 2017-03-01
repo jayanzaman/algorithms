382581863ef0700d19d3cb0c8e2732f43a3b1113
Jayan Zaman <jayanzaman@gmail.com>
2017-02-28 20:16:54 -0500

    adding weather api

---
+++ geoLocation/script.js
@@ -0,0 +1,35 @@
+function geoFindMe() {
+  var output = document.getElementById("out");
+
+  if (!navigator.geolocation){
+    output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
+    return;
+  }
+
+  function success(position) {
+    var latitude  = position.coords.latitude;
+    var longitude = position.coords.longitude;
+
+    output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
+
+    var img = new Image();
+    img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=14&size=1440x1440&sensor=false";
+
+    output.appendChild(img);
+  }
+
+  function error() {
+    output.innerHTML = "Unable to retrieve your location";
+  }
+
+  output.innerHTML = "<p>Locating…</p>";
+
+  navigator.geolocation.getCurrentPosition(success, error);
+}
+
+
+// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}
+
+var API_KEY = api.openweathermap.org/data/2.5/forecast?
+
+//  lat={lat}&lon={lon}
